c285c0c5b8efb5c684fcce7499a9f4430a2f1c2c
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-09-11 22:55:11 +0200

    move data files

--- digitsRecognizer/Program.cs
+++ digitsRecognizer/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 
 namespace digitsRecognizer
 {
@@ -10,6 +6,7 @@ namespace digitsRecognizer
     {
         static void Main(string[] args)
         {
+            File.ReadAllLines()
         }
     }
 }
